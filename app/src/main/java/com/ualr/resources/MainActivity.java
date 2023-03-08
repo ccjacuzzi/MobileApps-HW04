a7639fda8f3e525e9a3c670a5605c39f87d2b40e
Casey Jacuzzi <62916413+ccjacuzzi@users.noreply.github.com>
2023-03-08 14:47:29 -0600

    Changed build.gradle buildFeatures->viewBinding to true, then created the binding and inflated the layout. I had to go back and change the way the string was initialized for the country description. The assignment specification required the string to be initialized from the mainActivity java class rather than from the xml file. These changes have been made. All alternative resources are setup for the two languages. English is the default. Spanish is the alternate.

--- app/src/main/java/com/ualr/resources/MainActivity.java
+++ app/src/main/java/com/ualr/resources/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.ualr.resources.databinding.ActivityMainBinding;
 
 // TODO 3: Define the alternative resources needed to have different content and look n feel depending on the device language.
 // TODO 4: Get the description string value from resources
@@ -14,10 +15,16 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        this.mBinding.countryDescriptionText.setText(R.string.description);
     }
+
 
 }
